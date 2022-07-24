90a2903cff70b0f586d573dad1d2a1b0be5bd1a9
NitroLegionario <109915136+NitroLegionario@users.noreply.github.com>
2022-07-24 12:11:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ import { fileURLToPath } from 'url'
 
 //⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a | Add the numbers to be Owner ⋅.━━━━ ⊰
 global.owner = [
-  ['5212411347465', 'HADES_BOT', true],
-  ['5212412737467'],
-  ['5212411719888'],
-  ['5212411453522'],
+  ['51941125521', 'HADES_BOT', true],
+  ['51941125521'],
+  ['51996336184'],
+  ['51921912045'],
 ]
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
 
 global.mods = [] 
-global.prems = ['5212411347465'], 
+global.prems = ['51921912045'], 
 
 global.APIs = { // API Prefix.
   
